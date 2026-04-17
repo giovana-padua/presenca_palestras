05e1bbc7ef6f697ff099d3be7c3f9cf80ac8b34c
unknown <giogoncalvespadua13@gmail.com>
2026-04-17 10:57:20 -0300

    Desafio

---
+++ resources/views/dupla.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha dupla</title>
+</head>
+<body>
+    <h1>Dupla</h1>
+    <h3>Integrantes:</h3>
+    <p>Ana Carolina</p>
+    <p>Giovana</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clientes', function () {
+    return view('clientes');
+});
+
+Route::get('/inicio', [App\Http\Controllers\SiteController::class, 'index']);
+
+Route::get('/dupla', [App\Http\Controllers\Dupla::class, 'dupla'])
